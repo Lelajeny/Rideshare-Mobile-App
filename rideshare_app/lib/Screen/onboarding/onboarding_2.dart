16075ca14ab631faa2baee86fcdcaf40fef2aa69
Jennifer Harrison <101399233+Lelajeny@users.noreply.github.com>
2025-10-17 22:25:31 +0000

    updates on onboarding screens

---
+++ rideshare_app/lib/Screen/onboarding/onboarding_2.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:rideshare_app/core/configs/theme/app_colors.dart';
+
+class Onboarding2 extends StatefulWidget {
+  const Onboarding2({super.key});
+
+  @override
+  State<Onboarding2> createState() => _Onboarding2State();
+}
+
+class _Onboarding2State extends State<Onboarding2> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 50, left: 300),
+                child: TextButton(
+                  onPressed: () {
+                    // Handle skip action
+                  },
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xff414141),
+                    ),
+                  ),
+                ),
+              ),
+
+              Container(
+                margin: EdgeInsets.only(top: 60),
+                child: Column(
+                  children: [
+                    Center(child: Image.asset('assets/images/on2.png')),
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      child: Text(
+                        'At anytime',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: const Color(0xff414141),
+                        ),
+                      ),
+                    ),
+
+                    Container(
+                      margin: EdgeInsets.only(top: 20, left: 40, right: 40),
+                      child: Text(
+                        'Sell houses easily with the help of\n Listenoryx and to make this big\n I am writing more.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w500,
+                          color: const Color(0xffA0A0A0),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 40),
+
+                    // Circular button
+                    GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        height: 70,
+                        width: 70,
+                        decoration: BoxDecoration(
+                          //color: AppColors.primary,
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: AppColors.background,
+                            width: 4,
+                          ),
+                        ),
+
+                        child: Container(
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: AppColors.primary,
+                          ),
+                          child: const Center(
+                            child: Icon(
+                              Icons.arrow_forward,
+                              color: Color(0xff5A5A5A),
+                              size: 32,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    //const SizedBox(height: 20),
+
+                    // Bottom indicator (simulating iPhone bar)
+                    /**Container(
+                margin: const EdgeInsets.only(top: 8),
+                width: 120,
+                height: 4,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),**/
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- rideshare_app/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ rideshare_app/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/usr/local/share/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/new/Desktop/FLUTTER PROJECTS/GITHUB/Rideshare-Mobile-App/Rideshare-Mobile-App/rideshare_app"
+export "FLUTTER_ROOT=C:\flutter src\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Kiseki\Documents\GitHub\Rideshare-Mobile-App\rideshare_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_CLI_BUILD_MODE=debug"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
